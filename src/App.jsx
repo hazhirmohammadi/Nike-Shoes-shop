225e45971fe8e4108086b45a0676bdae5ce5e4d3
hazhirmohammadi <hazhir6690@gmail.com>
2023-10-22 12:25:11 +0330

    i added Header

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,46 @@
+import {
+   SuperQuality,
+   Footer,
+   CustomerReviews,
+   Services,
+   Hero,
+   SpecialOffer,
+   Subscribe,
+   PopularProduct
+} from "./sections/index.js";
+import Nav  from "./components/Nev.jsx";
 
-import './index.css'
 
-function App() {
+const App = () => (
+    <main className="relative">
+       <Nav/>
+       <section className="xl:padding-1 wide:padding-r padding-b ">
+          <Hero/>
+       </section>
+       <section className="padding ">
+          <PopularProduct/>
+       </section>
+       <section className="padding ">
+          <SuperQuality/>
+       </section>
+       <section className="padding ">
+          <Services/>
+       </section>
+       <section className="padding ">
+          <SpecialOffer/>
+       </section>
+       <section className="padding ">
+          <CustomerReviews/>
+       </section>
+       <section className="padding-x sm:py-32 py-16 w-full ">
+          <Subscribe/>
+       </section>
+       <section className="padding bg-black padding-x padding-t pb-8 ">
+          <Footer/>
+       </section>
+    </main>
+)
 
 
-  return (
-    <>
-     <div>
-        Hello !!
-     </div>
-    </>
-  )
-}
+export default App;
 
-export default App
---
+++ src/sections/index.js
@@ -0,0 +1,19 @@
+import Hero from "./Hero.jsx";
+import CustomerReviews from "./CustomerReviews.jsx";
+import SuperQuality from "./SuperQuality.jsx";
+import PopularProduct from "./PopularProduct.jsx";
+import Services from "./Services.jsx";
+import SpecialOffer from "./SpecialOffer.jsx";
+import Footer from "./Footer.jsx";
+import Subscribe from "./Subscribe.jsx";
+
+export {
+   Hero,
+    Services,
+    PopularProduct,
+    Footer,
+    SuperQuality,
+    Subscribe,
+    SpecialOffer,
+   CustomerReviews
+}
